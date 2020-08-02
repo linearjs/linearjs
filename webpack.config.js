ba8badca873029bbd5f26d0ef6acaa8cd7154bb0
matthyno <34276633+MatthyPlayz@users.noreply.github.com>
2020-08-02 15:57:35 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,5 @@ module.exports = {
     "bundle": "./src/linearjs.js",
     "bundle.min": "./src/linearjs.js",
   },
-  devtool: "source-map",
-  output: {
-    path: __dirname + "/build",
-    filename: "[name].js"
-  }
+  devtool: "source-map"
 };
